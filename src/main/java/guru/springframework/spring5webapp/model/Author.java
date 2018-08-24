23952f40d8cdf729c75e483545cfd7526952efa2
Rafał Świerczek <rafal.swierczek85@gmail.com>
2018-08-24 20:38:37 +0200

    jpa entities

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String firstname, String lastname, Set<Book> books) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
